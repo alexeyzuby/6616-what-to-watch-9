6f5532b13e5ccb1d445f9052eb36e7b1fb7765a8
azubarev <alexeyzuby@bk.ru>
2022-03-21 22:08:32 +0300

    Сделал на главной ссылку через span

--- project/src/components/logo/logo.tsx
+++ project/src/components/logo/logo.tsx
@@ -1,21 +1,34 @@
-import {Link} from 'react-router-dom';
+import {Link, useLocation} from 'react-router-dom';
 
 type LogoProps = {
   className?: string,
 }
 
 function Logo({className}: LogoProps): JSX.Element {
+  const location = useLocation();
   const logoClass = className ? `logo__link ${className}` : 'logo__link';
 
-  return (
-    <div className="logo">
-      <Link className={logoClass} to="/">
-        <span className="logo__letter logo__letter--1">W</span>
-        <span className="logo__letter logo__letter--2">T</span>
-        <span className="logo__letter logo__letter--3">W</span>
-      </Link>
-    </div>
-  );
+  if (location.pathname === '/') {
+    return (
+      <div className="logo">
+        <span className={logoClass}>
+          <span className="logo__letter logo__letter--1">W</span>
+          <span className="logo__letter logo__letter--2">T</span>
+          <span className="logo__letter logo__letter--3">W</span>
+        </span>
+      </div>
+    );
+  } else {
+    return (
+      <div className="logo">
+        <Link className={logoClass} to="/">
+          <span className="logo__letter logo__letter--1">W</span>
+          <span className="logo__letter logo__letter--2">T</span>
+          <span className="logo__letter logo__letter--3">W</span>
+        </Link>
+      </div>
+    );
+  }
 }
 
 export default Logo;
